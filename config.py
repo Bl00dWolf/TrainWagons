c83e1db7de69e6c29af2d914523230488e03dca3
Bl00dWolf <steamserver2@gmail.com>
2023-03-30 23:35:52 +0300

    Фикс описания в конфиге

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 # Этот файл для переменных среды, который в принципе не должен быть в Git как либо указан, но в данном случаи
 # он в Гите, т.к. это учебный проект и мы не боимся за безопасность
 
-AES_KEY: bytes = b'tz$x&SI#23C46Cx@VRgqAi5wDk9JqQLx'  # Должен быть либо 16 бит (символов) либо 32.
+AES_KEY: bytes = b'tz$x&SI#23C46Cx@VRgqAi5wDk9JqQLx'  # Должен быть либо 16 байт (символов) либо 32.
 CLIENT_IP, CLIENT_PORT = 'localhost', 2721
 SERVER_IP, SERVER_PORT = 'localhost', 2721
 SRV_TURNOFF_KEY = b'4[ygYqmEZ"cFx=IJ&^F+3?$HmVZR2R'
